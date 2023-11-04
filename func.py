ae6e0c27b3af1a9be92515a95a531f169d66f9df
Kauã Trindade <kauatrindade.dev@gmail.com>
2023-11-04 13:36:36 -0300

    mudei logica do funcao imc

--- func.py
+++ func.py
@@ -2,26 +2,23 @@ def imc(peso, altura):
 
     calculo = peso / (altura ** 2)
 
-    if calculo <= 18.5:
+    print(calculo)
+
+    if calculo < 18.5:
         return "Baixo peso"
     
-    elif calculo >= 18.5 or calculo <= 24.9:
+    elif calculo >= 18.5 and calculo <= 24.9:
         return 'Peso normal'
     
-    elif calculo >= 25 or calculo <= 29.9:
-        return 'Sobrepeso'
+    elif calculo > 25.0 and calculo <= 29.9:
+        return 'Excesso de peso'
     
-    elif calculo >= 30 or calculo <= 34.9:
-        return 'Obesidade grau 1'
+    elif calculo > 30.0 and calculo <= 34.9:
+        return 'Obesidade de classe 1'
     
-    elif calculo >= 35 or calculo <= 40:
-        return 'Obesidade grau 2'
-
-
-    elif calculo >= 40:
-        return 'Obesidade grau 3'
-
+    elif calculo > 35.0 and calculo <= 39.9:
+        return 'Obesidade de classe 2'
     
-    return 
-
-print(imc(peso = float(input('Qual seu peso? :')), altura = float(input('Qual sua altura? :'))))
+    return 'Obesidade de classe 3'
+    
+print(imc(peso = float(input('Qual seu peso ? :')), altura = float(input('Qual sua altura ? :'))))
